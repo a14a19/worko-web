f4eb9cd5c56179d31d833dca6d931530801030aa
a14a19 <anas31197@gmail.com>
2023-12-12 17:25:05 +0530

    updated code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import {
     CursorArrowRaysIcon,
     FingerPrintIcon,
     SquaresPlusIcon,
-    XMarkIcon,
+    XMarkIcon
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
@@ -31,7 +31,7 @@ function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <header className="bg-[#87878713] backdrop-blur max-w-[1140px] sticky top-0 rounded-full z-20 mx-auto mt-2  shadow-[inset_0px_0px_5px_rgba(225,225,225,0.1)]">
+        <header className="bg-[#87878713] backdrop-blur max-w-[1140px] sticky top-0 rounded-full z-20 mx-auto mt-2 shadow-[inset_0px_0px_5px_rgba(225,225,225,0.1)]">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-3" aria-label="Global">
                 <div className="flex me-5">
                     <a href="#" className="-m-1.5 p-1.5">
@@ -102,16 +102,19 @@ function Header() {
                     </Popover> */}
 
                     <a href="#" className="text-sm font-semibold leading-6 hover:text-white">
-                        Products
+                        Home
                     </a>
                     <a href="#" className="text-sm font-semibold leading-6 hover:text-white">
-                        Features
+                        Integrations
                     </a>
                     <a href="#" className="text-sm font-semibold leading-6 hover:text-white">
-                        Marketplace
+                        Pricing
                     </a>
                     <a href="#" className="text-sm font-semibold leading-6 hover:text-white">
-                        Company
+                        Docs
+                    </a>
+                    <a href="#" className="text-sm font-semibold leading-6 hover:text-white">
+                        Changelog
                     </a>
                 </Popover.Group>
                 <div className="hidden md:flex md:flex-1 md:justify-end">
@@ -120,18 +123,27 @@ function Header() {
                     </a>
                 </div>
             </nav>
-            <Dialog as="div" className="lg:hidden relative" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+            {
+                mobileMenuOpen
+                &&
+                <div className="md:hidden w-full h-full" onClick={() => setMobileMenuOpen(false)}>
+                    <div className='absolute top-0 z-40'>
+                        THIS IS MY CLASS
+                    </div>
+                </div>
+            }
+            {/* <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
                 <div className="fixed inset-0 z-30" />
-                <Dialog.Panel className="absolute right-5 z-30 w-full overflow-y-auto bg-[#121218] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10  rounded-3xl">
+                <Dialog.Panel className="fixed inset-0 right-5 z-30 w-full overflow-y-auto bg-[#121218] px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10  rounded-3xl">
                     <div className="flex items-center justify-between">
-                        {/* <a href="#" className="-m-1.5 p-1.5">
+                        <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Worko</span>
                             <img
                                 className="h-8 w-auto"
                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                                 alt=""
                             />
-                        </a> */}
+                        </a>
                         <button
                             type="button"
                             className="-m-2.5 rounded-md p-2.5"
@@ -144,54 +156,35 @@ function Header() {
                     <div className="mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
-                                {/* <Disclosure as="div" className="-mx-3">
-                                    {({ open }) => (
-                                        <>
-                                            <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 hover:bg-gray-50">
-                                                Product
-                                                <ChevronDownIcon
-                                                    className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
-                                                    aria-hidden="true"
-                                                />
-                                            </Disclosure.Button>
-                                            <Disclosure.Panel className="mt-2 space-y-2">
-                                                {[...products, ...callsToAction].map((item) => (
-                                                    <Disclosure.Button
-                                                        key={item.name}
-                                                        as="a"
-                                                        href={item.href}
-                                                        className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 hover:bg-gray-50"
-                                                    >
-                                                        {item.name}
-                                                    </Disclosure.Button>
-                                                ))}
-                                            </Disclosure.Panel>
-                                        </>
-                                    )}
-                                </Disclosure> */}
                                 <a
                                     href="#"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:text-white"
                                 >
-                                    Products
+                                    Home
                                 </a>
                                 <a
                                     href="#"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:text-white"
                                 >
-                                    Features
+                                    Integrations
                                 </a>
                                 <a
                                     href="#"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:text-white"
                                 >
-                                    Marketplace
+                                    Pricing
                                 </a>
                                 <a
                                     href="#"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:text-white"
                                 >
-                                    Company
+                                    Docs
+                                </a>
+                                <a
+                                    href="#"
+                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 hover:text-white"
+                                >
+                                    Changelog
                                 </a>
                             </div>
                             <div className="py-6">
@@ -205,7 +198,7 @@ function Header() {
                         </div>
                     </div>
                 </Dialog.Panel>
-            </Dialog>
+            </Dialog> */}
         </header>
     )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,18 +1,27 @@
+import { SparklesIcon } from '@heroicons/react/24/outline'
+
 function HeroSection() {
     return (
         <aside>
-            <button>
-                Made By Vite + React
-            </button>
-            <h1 className="text-center">
-                The best platform for cross-<br /> <span>functional work.</span>
-            </h1>
-            <p>
-                Want better results in your organization? Taskify helps teams get clarity, achieve greater impact, and scale to meet company goals.
-            </p>
+            <div className="h-[486px] flex items-center justify-between flex-col py-20" style={{backgroundImage: "radial-gradient(45.6% 68.2% at 50% 100%,#7808ff 0%,rgba(3,6,13,0) 100%)"}}>
+                <div className="flex items-center justify-between flex-col gap-y-8">
+                    <button className="flex items-center bg-[#121218] text-xs py-2 px-4 rounded-full shadow-[inset_0px_0px_5px_rgba(225,225,225,0.1)]">
+                        <SparklesIcon className='h-4 me-3' />
+                        Made By Vite + React
+                    </button>
+                    <h1 className="text-center text-5xl font-medium text-slate-200">
+                        The best platform for cross-<br /> <span className='text-gray-500'>functional work.</span>
+                    </h1>
+                    <p className='max-w-[640px] text-center text-base font-light'>
+                        Want better results in your organization? Worko helps teams get clarity, achieve greater impact, and scale to meet company goals.
+                    </p>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <button className='me-2 text-base font-semibold bg-[#7808ff] text-white rounded-full px-5 py-3'>Get started</button>
+                    <button className='ms-2 text-base font-semibold hover:bg-[#933aff20] text-white rounded-full px-5 py-3'>See how it works</button>
+                </div>
+            </div>
             <div>
-                <button>Get started</button>
-                <button>See how it works</button>
             </div>
         </aside>
     )
