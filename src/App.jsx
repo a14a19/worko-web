057ba9bbbf9af942dc15f6cfa22aeea89a5da493
a14a19 <anas31197@gmail.com>
2023-12-12 14:23:45 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import Home from "./pages/Home"
+
+function App() {
+
+  return (
+    <main className="p-5">
+      <Header />
+      <Home />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,21 @@
+function HeroSection() {
+    return (
+        <aside>
+            <button>
+                Made By Vite + React
+            </button>
+            <h1 className="text-center">
+                The best platform for cross-<br /> <span>functional work.</span>
+            </h1>
+            <p>
+                Want better results in your organization? Taskify helps teams get clarity, achieve greater impact, and scale to meet company goals.
+            </p>
+            <div>
+                <button>Get started</button>
+                <button>See how it works</button>
+            </div>
+        </aside>
+    )
+}
+
+export default HeroSection
