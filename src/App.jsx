0bbfccbc3d68886906ac8126c002e69847d23e90
Anas Ahmed <anas31197@gmail.com>
2023-12-14 09:56:10 +0530

    commited (#1)
    Co-authored-by: chandni <chandnimistry33@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import Header from "./components/Header"
-import Home from "./pages/Home"
+import Header from "./components/Header";
+import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Changelog from "./components/Changelog/Changelog";
 
 function App() {
-
   return (
     <main className="p-5">
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/changelog" element={<Changelog />} />
       </Routes>
     </main>
-  )
+  );
 }
 
 export default App;
