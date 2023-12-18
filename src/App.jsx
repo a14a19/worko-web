b42690e78eeb641fe1e8aa1fb1de616108c45e54
a14a19 <anas31197@gmail.com>
2023-12-18 11:03:39 +0530

    worko bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 // import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import { useLocation } from "react-router-dom";
+import { useLayoutEffect } from "react";
 
 function App() {
+
+  const location = useLocation()
+  const currentPath1 = location.pathname.split("/")[1];
+  const currentPath2 = location.pathname.split("/")[2];
+
+  useLayoutEffect(() => {
+
+    return () => window.scrollTo({
+      top: 0,
+      opacity: 0,
+      behavior: "smooth",
+    });
+
+  }, [currentPath1, currentPath2])
+
+
   return (
     <main className="px-5">
       <Routes>
