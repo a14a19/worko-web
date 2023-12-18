bd94de98c7bcd4b402a48a828e23b6ea8834210f
Anas Ahmed <anas31197@gmail.com>
2023-12-18 17:31:39 +0530

    docs (#4)

---
+++ src/components/docs/ReportAnalytics.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+
+function ReportAnalytics() {
+    return (
+        <div  style={{ padding: "revert", margin: "revert", fontSize: "revert",  }}>
+            <p>Advanced features</p>
+            <h1>Reporting and Analytics</h1>
+            <p> Reports and analytics help you gain insights into your task management and track progress. In this section, we'll explore how to generate reports on task completion in Worko.</p>
+
+            <br />
+
+            <hr></hr>
+
+
+            <h3>Generating a Task Completion Report</h3>
+            <p>To generate a report on task completion in Worko, follow these steps: <br />
+                Step 1: Log in to your Worko Management account. <br />
+
+                Step 2: Navigate to the "Reports" or "Analytics" section in your Worko dashboard. <br />
+
+                Step 3: Select the type of report you want to generate, such as "Task Completion Report." <br />
+
+                Step 4: Choose the date range or criteria for the report. For example, you can select a specific time frame or filter tasks by status. <br />
+
+                Step 5: Click the "Generate Report" or similar button to create the report. <br />
+
+                Step 6: View and download the generated report, which may include statistics on completed tasks, task duration, and more. <br />
+
+                <h3>Using the Code Block</h3>
+
+                <p>To programmatically generate a task completion report using the Worko API, you can use the following code example: <br />
+
+
+
+                    This code sends a request to the Worko API to generate a report on task completion for the specified date range and status. <br />
+
+                    <br />
+
+                    By following these steps or using the provided code block, you can generate task completion reports to assess your task management performance in Worko.
+                </p>
+
+
+
+
+
+
+
+
+
+
+                <div>
+
+                </div>
+
+
+
+
+
+            </p>
+
+            <br />
+
+            <br />
+
+            <hr style={{ border: "1px solid grey" }} />
+
+
+            <div>
+                <footer>
+
+                    <br />
+                    © Copyright 2023. All rights reserved.
+
+                </footer>
+            </div>
+
+
+
+
+
+
+        </div >
+    )
+}
+
+export default ReportAnalytics
---
+++ src/components/docs/SetUpIntegrations.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+
+function SetUpIntegrations() {
+    return (
+        <div  style={{ padding: "revert", margin: "revert", fontSize: "revert",  }}>
+            <p>Integrations</p>
+            <h1>Set Up Integrations</h1>
+            <p> Worko offers seamless integration with various external tools and services to enhance your workflow and task <br /> management experience. In this section, we'll guide you through the process of setting up integrations with <br /> Worko.</p>
+
+            <br />
+
+            <hr></hr>
+
+
+            <h3>Supported Integrations</h3>
+            <p> Worko supports integrations with a variety of popular tools and services, allowing you to connect your tasks and projects <br /> with your favorite apps. Some of our supported integrations include:</p>
+
+            <ul>
+                <li>Slack: Integrate with Slack to receive task notifications and updates in your Slack channels</li>
+                <li>
+                    Google Calendar: Sync your Worko tasks and deadlines with Google Calendar for better time management.</li>
+                <li>Trello: Connect Worko with Trello to manage tasks seamlessly across both platforms.</li>
+            </ul>
+
+            <h3>Setting Up an Integration</h3>
+            <p>Here's a step-by-step guide on how to set up an integration with Worko. We'll use the example of setting up a Slack integration:</p>
+            <p>Step 1: In the "Account Settings" page, navigate to the "Security" section. <br />
+                Step 2: Find the "Change Password" option and click it. <br />
+                Step 3: Enter your current password and your new password in the provided fields. <br />
+                Step 4: Click the "Save Changes" button to update your password.
+            </p>
+
+            <h3>Managing Notifications</h3>
+            <p>Worko allows you to customize your notification preferences. To do so, follow these steps: <br />
+                Step 1: In the "Account Settings" page, navigate to the "Notifications" section. <br />
+                Step 2: Adjust your notification settings according to your preferences.
+
+                <h3>Deleting Your Account</h3>
+                <p>Should you decide to delete your Worko account, follow these steps: <br />
+
+                    Step 1: In the "Account Settings" page, scroll down to the "Account Deletion" section. <br />
+                    Step 2: Click the "Delete Account" button.
+                    Step 3: Follow the on-screen instructions to confirm and complete the account deletion process.
+
+                    <p>By following these steps, you can easily set up and configure integrations with external services to enhance your Worko experience.</p>
+
+
+
+
+
+
+
+                </p>
+
+
+
+            </p>
+
+            <br />
+
+            <br />
+
+            <hr style={{ border: "1px solid grey" }} />
+
+
+            <div>
+                <footer>
+
+                    <br />
+                    © Copyright 2023. All rights reserved.
+
+                </footer>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default SetUpIntegrations
