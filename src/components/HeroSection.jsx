58e830c797153d8d3a3a1573711021af1e683a09
a14a19 <anas31197@gmail.com>
2023-12-20 00:38:27 +0530

    vite + react removed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -29,10 +29,10 @@ function HeroSection() {
         <aside className='max-w-[1140px] min-h-min mx-auto my-20' ref={animaton}>
             <div className="h-[486px] flex items-center justify-between flex-col py-20" style={{ backgroundImage: "radial-gradient(50.0% 85.0% at 50% 100%,rgb(120 8 255 / 70%) 0%,rgba(3,6,13,0) 70%)" }}>
                 <div className="flex items-center justify-between flex-col gap-y-8 max-w-[720px]" onClick={() => toggleAnswer()}>
-                    <button className="flex items-center bg-[#121218] text-xs py-2 px-4 rounded-full shadow-[inset_0px_0px_5px_rgba(225,225,225,0.1)]">
+                    {/* <button className="flex items-center bg-[#121218] text-xs py-2 px-4 rounded-full shadow-[inset_0px_0px_5px_rgba(225,225,225,0.1)]">
                         <SparklesIcon className='h-4 me-3' />
                         Made By Vite + React
-                    </button>
+                    </button> */}
                     <h1 className="text-center md:text-5xl sm:text-2xl text-xl font-medium text-slate-200 bg-clip-text" style={{ backgroundImage: "linear-gradient(0deg, rgb(97, 100, 107) 0%, rgb(255, 255, 255) 100%)", WebkitTextFillColor: "transparent" }}>
                         The best platform for cross-functional work.
                     </h1>
